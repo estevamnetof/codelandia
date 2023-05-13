454b7dcca19299c6d13cf231164fdd409691909a
Estevam Neto <estevam.neto07@gmail.com>
2023-05-12 23:25:22 -0300

    mudanças desafio 1

--- desafio-01/script.js
+++ desafio-01/script.js
@@ -1,5 +1,10 @@
 const icon = document.querySelectorAll('#icon');
 
+function handleClick(event) {
+    console.log(event.target.getAttribute('src'))
+    event.target.src = 'images/heart-full.svg'
+}
 
-
-
+icon.forEach(item => {
+    item.addEventListener('click', handleClick);
+});
