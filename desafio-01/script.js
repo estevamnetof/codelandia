99469a00ae1130a0aeceea2f440d11865642ce1c
estevamnetof <estevam.neto07@gmail.com>
2023-05-26 12:23:47 -0300

    tentando colocar campo de busca no javascript

--- desafio-01/script.js
+++ desafio-01/script.js
@@ -1,9 +1,16 @@
+// Preenchimento do coração
 const icon = document.querySelectorAll('#icon');
 
 function handleClick() {
-    this.classList.toggle('filled')
+    this.classList.toggle('filled');
 }
 
 icon.forEach(item => {
     item.addEventListener('click', handleClick);
 });
+
+// Pesquisa
+const busca = document.querySelector('.header-busca input').value;
+const conteudo = document.querySelectorAll('.conteudo');
+const conteudoArray = Array.from(conteudo);
+
