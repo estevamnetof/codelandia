c8fcd3a39b98c63b16ba874d73c210fd0c8d1398
estevamnetof <estevam.neto07@gmail.com>
2023-07-30 03:51:24 -0300

    desafio 01 - implementando busca

--- desafio-01/script.js
+++ desafio-01/script.js
@@ -10,7 +10,37 @@ icon.forEach(item => {
 });
 
 // Pesquisa
-const busca = document.querySelector('.header-busca input').value;
+const busca = document.querySelector('.header-busca input');
 const conteudo = document.querySelectorAll('.conteudo');
-const conteudoArray = Array.from(conteudo);
+const conteudoH2 = document.querySelectorAll('.conteudo h2');
 
+const conteudoArray = [];
+
+conteudoH2.forEach((elemento) => {
+    const objetoH2 = {
+        text: elemento.textContent
+    };
+
+    conteudoArray.push(objetoH2);
+});
+
+busca.addEventListener('keydown', handleSearch);
+
+function handleSearch() {
+    const pesquisa = busca.value;
+
+    const resultado = conteudoArray.filter(item => {
+        return item.text.toLocaleLowerCase().includes(pesquisa.toLocaleLowerCase());
+    });
+
+    if (pesquisa === '') return;
+
+    resultado.forEach(item => {
+        const elementoEncontrado = conteudo.find(elemento => elemento.textContent.toLocaleLowerCase() === item.text.toLocaleLowerCase());
+        if (elementoEncontrado) {
+            elementoEncontrado.classList.add('green');
+        }
+    });
+
+    console.log(resultado);
+}
